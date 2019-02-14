28a7abf75b40eec98f44b3c6442340eef2d43918
Lance0404 <virtuouslycan@gmail.com>
2019-02-14 15:20:47 +0800

    donnot upload docker*/

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -4,5 +4,5 @@ SELECT pg_terminate_backend(pg_stat_activity.pid)
       AND pid <> pg_backend_pid();
 DROP DATABASE break_content;
 CREATE DATABASE break_content;
-CREATE USER breaktime WITH ENCRYPTED PASSWORD 'breaktime';
-GRANT ALL PRIVILEGES ON DATABASE break_content TO breaktime;
+-- CREATE USER breaktime WITH ENCRYPTED PASSWORD 'breaktime';
+-- GRANT ALL PRIVILEGES ON DATABASE break_content TO breaktime;

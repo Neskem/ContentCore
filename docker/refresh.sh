7d80ed9f9a787999fde7122147831a83eaf617e3
Lance0404 <virtuouslycan@gmail.com>
2019-04-12 10:02:38 +0800

    save

--- docker/refresh.sh
+++ docker/refresh.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-# PGPASSWORD=admin psql -U postgres -h 192.168.18.111 < docker/postgresql/init.sql
-# docker volume create --name=data
 
 docker-compose down -v
-# docker build -t cc -f ../Dockerfile ../
 docker-compose build
 docker-compose up -d
-# manual garbage collection
 docker rmi $(docker images -f "dangling=true" -q)
-# cat breakcontent.env | grep -v '^#' | grep -v '^$' | awk '{print "export "$0}'
-# docker-compose restart nginx
-# psql -U postgres -h localhost < postgresql/init.sql
+# ------------------ START init DB ------------------
 psql -h localhost -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'break_content' AND pid <> pg_backend_pid()"
 psql -h localhost -U postgres -c 'DROP DATABASE break_content'
 psql -h localhost -U postgres -c 'CREATE DATABASE break_content'
-
-# docker-compose restart
+# ------------------ END ------------------
 docker-compose ps
 curl -v -X GET 'http://localhost:8100/v1/create_tasks/1'
-# check files in container
-# docker exec -it web ls -al
-# copy & paste manually
+

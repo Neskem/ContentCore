c0e07344e1875129b69745e055a3888f5784f798
Alan_Liu (RD) <alan@breaktime.com.tw>
2019-05-03 16:37:37 +0800

    Modify stage environment for setting configure, part2.

--- docker_stg/refresh.sh
+++ docker_stg/refresh.sh
@@ -10,15 +10,15 @@ if [ "$dropdb" == "drop" ]; then
     docker-compose up -d
     sleep 5 # takes time for containter be ready
     if [ -e "/etc/os-release" ]; then
-        psql -h 10.140.15.248 -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'break_content' AND pid <> pg_backend_pid()"
-        psql -h 10.140.15.248 -U postgres -c 'DROP DATABASE break_content'
+        PGPASSWORD=ContentBreak_psql1qaz psql -h 10.140.15.248 -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'break_content' AND pid <> pg_backend_pid()"
+        PGPASSWORD=ContentBreak_psql1qaz psql -h 10.140.15.248 -U postgres -c 'DROP DATABASE break_content'
         sleep 3
-        psql -h 10.140.15.248 -U postgres -c 'CREATE DATABASE break_content'
+        PGPASSWORD=ContentBreak_psql1qaz psql -h 10.140.15.248 -U postgres -c 'CREATE DATABASE break_content'
     else
-        PGPASSWORD=admin /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'break_content' AND pid <> pg_backend_pid()"
-        PGPASSWORD=admin /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c 'DROP DATABASE break_content'
+        PGPASSWORD=ContentBreak_psql1qaz /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'break_content' AND pid <> pg_backend_pid()"
+        PGPASSWORD=ContentBreak_psql1qaz /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c 'DROP DATABASE break_content'
         sleep 3
-        PGPASSWORD=admin /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c 'CREATE DATABASE break_content'
+        PGPASSWORD=ContentBreak_psql1qaz /Applications/Postgres.app/Contents/Versions/10/bin/psql -U postgres -h 10.140.15.248 -c 'CREATE DATABASE break_content'
     fi
 fi
 docker-compose up -d

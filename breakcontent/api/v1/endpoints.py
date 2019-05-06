ce4ce1c292538436f9845bf649fb1d23a245d3fe
Alan_Liu (RD) <alan@breaktime.com.tw>
2019-05-06 11:30:12 +0800

    Add docker_prd for migrating docker_web and docker_worker those two environment.

--- breakcontent/api/v1/endpoints.py
+++ breakcontent/api/v1/endpoints.py
@@ -2,12 +2,11 @@ from flask import Blueprint, request, g, jsonify, current_app, abort
 from flask_headers import headers
 from flask_cors import cross_origin
 from breakcontent import db
-import json
 from breakcontent.api import errors
-import datetime
 from sqlalchemy.orm import joinedload, Load, load_only
+from breakcontent.models import TaskMain, WebpagesPartnerXpath, DomainInfo
+import datetime
 
-from breakcontent.models import TaskMain, TaskService, TaskNoService, WebpagesPartnerXpath, WebpagesPartnerAi, WebpagesNoService, StructureData, UrlToContent, DomainInfo, BspInfo
 
 bp = Blueprint('endpoints', __name__)
 
@@ -65,11 +64,8 @@ def init_task():
 @cross_origin()
 def delete_task():
     """
-    # delete partner from maintask
+    # delete record from maintask
     curl -v -X DELETE 'http://localhost:80/v1/delete_task' -H 'Content-Type: application/json' -d '{"url_hash": "a6d62aaef4856b23d7d8016e4e77409001d999fa"}'
-
-    # delete nopartner from maintask
-    curl -v -X DELETE 'http://localhost:80/v1/delete_task' -H 'Content-Type: application/json' -d '{"url_hash": "test2"}'
     """
 
     res = {'msg': '', 'status': False}
@@ -88,11 +84,6 @@ def delete_task():
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def create_tasks(priority):
-    '''
-    # delete partner from maintask
-    curl -v -X GET 'http://localhost:80/v1/create_tasks/1'
-    '''
-
     res = {'msg': '', 'status': False}
     data = request.json
 
@@ -109,25 +100,17 @@ def create_tasks(priority):
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def get_content(url_hash):
-    '''
+    """
     AC requesting article
-
     this is a sync function
-
-    <example>
     curl -v -X GET -H 'Content-Type: application/json' 'http://localhost:80/v1/content/a6d62aaef4856b23d7d8016e4e77409001d999fa'
-
-    curl -v -X GET -H 'Content-Type: application/json' 'http://localhost:80/v1/content/2151238f566088dd8f5b56857938f28dced4d899'
-
-    '''
+    """
 
     res = {'msg': '', 'status': False}
 
     wpxf = WebpagesPartnerXpath.query.filter_by(url_hash=url_hash).first()
     data = {'data': wpxf.to_ac()}
     res.update(data)
-    # wpxf_json = json.dumps(wpxf.to_ac())
-    # current_app.logger.debug(f'wpxf_json {wpxf_json}')
     res.update({
         'msg': 'ok',
         'status': True
@@ -144,12 +127,7 @@ def get_content(url_hash):
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def partner_setting_add_update(partner_id, domain):
-    '''
-    curl -v -X PUT -H 'Content-Type: application/json' 'http://localhost:80/v1/partner/setting/3WYST18/www.kocpc.com.tw' -d '{"xpath": "blablabla"}'
-
-    this is a sync func
-    '''
-    current_app.logger.debug('run partner_setting_add_update()...')
+    current_app.logger.debug(f'domain{domain}, partner_id{partner_id}, run partner_setting_add_update()...')
 
     q = dict(domain=domain, partner_id=partner_id)
     data = request.json  # data should be a dict
@@ -160,15 +138,6 @@ def partner_setting_add_update(partner_id, domain):
     di = DomainInfo()
     di.upsert(q, idata)
 
-    # di = DomainInfo.query.filter_by(**q).first()
-    # if di:
-    #     # update no matter what
-    #     db_session_update(db.session, DomainInfo, q, idata)
-    # else:
-    #     # insert
-    #     doc = DomainInfo(**idata)
-    #     db_session_insert(db.session, doc)
-
     res = {'msg': 'ok', 'status': True}
     return jsonify(res), 200
 
@@ -177,18 +146,13 @@ def partner_setting_add_update(partner_id, domain):
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def health_check(itype: str=None):
-    '''
+    """
     this is a sync function
-
     daily health check
-
     curl -v -X GET -H 'Content-Type: application/json' 'http://localhost:8100/v1/hc/all'
-
     curl -v -X GET -H 'Content-Type: application/json' 'http://localhost:8100/v1/hc/day'
-
     curl -v -X GET -H 'Content-Type: application/json' 'http://localhost:8100/v1/hc/hour'
-
-    '''
+    """
     current_app.logger.debug('run health_check()...')
     res = {'msg': '', 'status': False}
 
@@ -212,13 +176,11 @@ def health_check(itype: str=None):
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def get_pd(partner_id, domain):
-    '''
-    a sync function
-
+    """
+    this is a sync function
     too much request might halt my system
-
     return url_hash, publish_date to AC
-    '''
+    """
     current_app.logger.debug('run get_pd()...')
     res = {'msg': '', 'status': False}
 
@@ -252,7 +214,6 @@ def get_pd(partner_id, domain):
             data.append(adict)
 
     if 1:
-        # raw sql
         sql_str = f'select a.url_hash, b.publish_date from task_main as a join webpages_partner_xpath as b on a.url_hash = b.url_hash where a.partner_id = \'{partner_id}\' and a.domain = \'{domain}\';'
 
         current_app.logger.debug(f'sql_str {sql_str}')
@@ -261,13 +222,10 @@ def get_pd(partner_id, domain):
         current_app.logger.debug(f'wp_list {wp_list}')
         data = []
         for i in wp_list:
-            # i is object of TaskMain
             adict = {}
             adict['url_hash'] = i['url_hash']
             adict['publish_date'] = i['publish_date']
             data.append(adict)
-
-    # current_app.logger.debug(f'wp_list {wp_list}')
 
     res.update({
         'msg': 'ok',
@@ -276,16 +234,14 @@ def get_pd(partner_id, domain):
     })
     return jsonify(res), 200
 
-# ===== Below are for debug use =====
-
 
 @bp.route('/error/<etype>', methods=['GET'])
 @headers({'Cache-Control': 's-maxage=0, max-age=0'})
 @cross_origin()
 def error_handler(etype):
-    '''
+    """
     curl -v -X GET 'http://localhost:80/v1/error/2'
-    '''
+    """
     res = {'msg': '', 'status': False}
 
     current_app.logger.debug(f'type {type(etype)}')
@@ -297,24 +253,5 @@ def error_handler(etype):
         abort(404)
     elif etype == 3:
         raise errors.InvalidUsage('invalid usage', status_code=410)
-    elif etype == 4:
-        adict = {'a': 'aaa'}
-        # try:
-        #     tmp = adict['b']
-        # except Exception as e:
-        #     raise
-        tmp = adict['b']
-    # elif etype == 5:
-    #     current_app.logger.debug('alan error test')
-    #     try:
-    #         alan.sub_error()
-    #     except:
-    #         pass
-    #     # aec = AlanErrorClass()
-    #     # try:
-    #     #     aec.sub_error()
-    #     # except Exception as e:
-    #     #     pass
-    #     return jsonify(res), 200
     elif etype == 6:
         pass

06fc6bf2db9664b754ae34d53725713310c5401e
FlynnSun <flynnsun@FlynnSunde-MacBook-Pro.local>
2019-09-23 11:19:35 +0800

    None partner version

---
+++ install_mercury.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ./mercury-parser
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install nodejs
+apt-get -r install git
+git clone https://github.com/postlight/mercury-parser.git .
+# shellcheck disable=SC2164
+cd ./mercury-parser
+npm install
+

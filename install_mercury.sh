790c2bdfbad85aff96a39fab60a62eebdb0a57c0
Flynn <flynn@breaktime.com.tw>
2019-10-16 14:42:33 +0800

    Add new environment for idc

--- install_mercury.sh
+++ install_mercury.sh
@@ -4,7 +4,7 @@ rm -rf ./mercury-parser
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install nodejs
 apt-get -r install git
-git clone https://github.com/postlight/mercury-parser.git .
+git clone https://github.com/postlight/mercury-parser.git
 # shellcheck disable=SC2164
 cd ./mercury-parser
 npm install

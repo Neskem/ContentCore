a30aeab5d8161e3ecc1302c5009f781b75a094ce
Flynn <flynn@breaktime.com.tw>
2019-10-17 17:41:24 +0800

    Fix a bug

--- install_mercury.sh
+++ install_mercury.sh
@@ -3,7 +3,7 @@
 rm -rf ./mercury-parser
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install nodejs
-apt-get -r install git
+apt-get -y install git
 git clone https://github.com/postlight/mercury-parser.git
 # shellcheck disable=SC2164
 cd ./mercury-parser

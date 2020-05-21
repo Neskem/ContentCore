0344ee9639d81bd7e41605361a4c83012d58f981
Flynn <flynn@breaktime.com.tw>
2020-05-21 15:17:17 +0800

    Remove redundant code

--- docker/build_image.sh
+++
@@ -1,9 +0,0 @@
-
-docker-compose down -v
-
-docker build -t cc -f ../Dockerfile ../
-
-docker rmi $(docker images -f "dangling=true" -q)
-
-# pack image
-docker save -o /home/lance/playground/cc.tar cc
